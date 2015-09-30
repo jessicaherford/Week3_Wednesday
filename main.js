41f3999a142b78ae7387712c45dce4ef1cea8bfa
Jenny Allar <jennyallar@gmail.com>
2015-09-30 14:53:34 -0600

    adding a few exercises

---
+++ main.js
@@ -0,0 +1,19 @@
+// More Function Exercises!
+
+// 1. Write a function that prints out the entire "99 Bottles of Beer on the Wall" song lyrics.
+
+// 2. Use a function to ask a user for a temperature in Celsius and converts it to Fahrenheit.
+
+// 3. Adding to the code in #3, prompt the user a second time to determine whether they'd like to convert from Fahrenheit to Celsius
+//    or from Celsius to Fahrenheit. To keep it simple, instruct them to use "F to C" or "C to F".
+//    Alert them with an error if they do not follow the rules.
+
+// 4. Create a 'Guessing Game'. The game starts by picking a random number.
+//    It then prompts the user to guess the number. If the user's number is lower/higher,
+//    it will prompt the user to enter another guess and tell the user if the guess was
+//    too high or too low. This continues until the correct guess is entered.
+//    When the correct guess is entered the user is given a success message with the correct number.
+
+
+//  . http://games.usvsth3m.com/javascript-under-pressure/
+//    Have fun with these! Get as far as you can and record your progress and time. We'll try this again in a few weeks!
